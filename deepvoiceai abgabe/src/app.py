07386e0ae41c59975fa175ada9b22c284e9104c1
Mighel <39377603+FreZZyBuilds@users.noreply.github.com>
2026-04-18 05:04:38 +0200

    Delete deepvoiceai abgabe directory

--- deepvoiceai abgabe/src/app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""DeepVoice Pro v5.0 - Backend API"""
-from backend_app_v5_real_detection import app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
